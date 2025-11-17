96de7f32201c9f79df601afaa133f4ddcaa6a679
Matthew Pickering <matthewtpickering@gmail.com>
2025-11-17 15:23:29 +0000

    Fix fibber-c-main function

--- examples/fibber-c-main/cbits/fibber-c-main.c
+++ examples/fibber-c-main/cbits/fibber-c-main.c
@@ -14,10 +14,13 @@ int main (int argc, char *argv[])
 
     // Enable RTS options:
     conf.rts_opts_enabled = RtsOptsAll;
+    conf.rts_opts = "-l";
 
     // Start the eventlog writer:
     const char* sock_path = getenv("GHC_EVENTLOG_SOCKET");
     eventlog_socket_init(sock_path);
+    // Wait for the monitoring process to connect.
+    eventlog_socket_wait();
 
     extern StgClosure ZCMain_main_closure;
     return hs_main(argc, argv, &ZCMain_main_closure, conf);
--- examples/fibber-c-main/fibber-c-main.sh
+++ examples/fibber-c-main/fibber-c-main.sh
@@ -1,20 +1,20 @@
 #!/bin/sh -e
 
 # Set the eventlog socket
-export FIBBER_EVENTLOG_SOCKET="/tmp/fibber_eventlog.sock"
+export GHC_EVENTLOG_SOCKET="/tmp/fibber_eventlog.sock"
 FIBBER_EVENTLOG="/tmp/fibber.eventlog"
 
 # Build fibber
-cabal build fibber
+cabal build fibber-c-main
 
 # Run fibber
-cabal run fibber -- 24 &
+cabal run fibber-c-main -- 24 &
 FIBBER_PID=$!
 
 # Run NetCat
 # NOTE: The purpose of 'sleep 5' is to give the fibber process
 #       sufficient time to create the Unix socket.
-sleep 5 && nc -U "$FIBBER_EVENTLOG_SOCKET" >"$FIBBER_EVENTLOG"
+sleep 1 && nc -U "$GHC_EVENTLOG_SOCKET" >"$FIBBER_EVENTLOG"
 
 # Wait for fibber to finish
 wait $FIBBER_PID
