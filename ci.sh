dc2ff7ef3550a7dccbe84562935483e8a287066e
Matthew Pickering <matthewtpickering@gmail.com>
2025-11-18 11:12:28 +0000

    Make fibber test run for slightly longer

--- ci.sh
+++ ci.sh
@@ -18,9 +18,9 @@ run_step() {
     fi
 }
 
-run_step "Test fibber (finite)" "$TESTS_DIR/test-normal.sh" fibber 24
+run_step "Test fibber (finite)" "$TESTS_DIR/test-normal.sh" fibber 30
 run_step "Test fibber (forever, with reconnect)" "$TESTS_DIR/test-reconnect.sh" fibber --forever 24
-run_step "Test fibber-c-main (finite)" "$TESTS_DIR/test-normal.sh" fibber-c-main 24
+run_step "Test fibber-c-main (finite)" "$TESTS_DIR/test-normal.sh" fibber-c-main 30
 run_step "Test fibber-c-main (forever, with reconnect)" "$TESTS_DIR/test-reconnect.sh" fibber-c-main --forever 24
 
 printf '\nAll requested steps completed.\n'
