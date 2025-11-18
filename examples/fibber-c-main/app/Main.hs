4b87ee9a8c7ee66628a3c9d6b2e90a303ca877a4
Matthew Pickering <matthewtpickering@gmail.com>
2025-11-18 10:48:26 +0000

    Rework testing scripts

--- examples/fibber-c-main/app/Main.hs
+++ examples/fibber-c-main/app/Main.hs
@@ -1,15 +1,26 @@
 module Main where
 
+import Control.Monad (forever)
 import Data.Foldable (for_)
 import GHC.Eventlog.Socket (wait)
 import System.Environment (getArgs)
+
+data Mode = Finite | Infinite
+
+parseArgs :: [String] -> (Mode, [String])
+parseArgs ("--forever" : rest) = (Infinite, rest)
+parseArgs args = (Finite, args)
 
 main :: IO ()
 main = do
     wait
     args <- getArgs
-    for_ args $ \arg ->
-        print (fib (read arg))
+    let (mode, fibArgs) = parseArgs args
+        workload = for_ fibArgs $ \arg -> print (fib (read arg))
+    case (mode, fibArgs) of
+        (_, []) -> putStrLn "Provide at least one integer argument."
+        (Finite, _) -> workload
+        (Infinite, _) -> forever workload
 
 fib :: Integer -> Integer
 fib 0 = 0
--- examples/fibber/app/Main.hs
+++ examples/fibber/app/Main.hs
@@ -1,9 +1,16 @@
 module Main where
 
+import Control.Monad (forever)
 import Data.Foldable (for_)
 import Data.Maybe (fromMaybe)
 import GHC.Eventlog.Socket
 import System.Environment
+
+data Mode = Finite | Infinite
+
+parseArgs :: [String] -> (Mode, [String])
+parseArgs ("--forever" : rest) = (Infinite, rest)
+parseArgs args = (Finite, args)
 
 main :: IO ()
 main = do
@@ -12,8 +19,12 @@ main = do
             <$> lookupEnv "FIBBER_EVENTLOG_SOCKET"
     startWait fibberEventlogSocket
     args <- getArgs
-    for_ args $ \arg ->
-        print (fib (read arg))
+    let (mode, fibArgs) = parseArgs args
+        workload = for_ fibArgs $ \arg -> print (fib (read arg))
+    case (mode, fibArgs) of
+        (_, []) -> putStrLn "Provide at least one integer argument."
+        (Finite, _) -> workload
+        (Infinite, _) -> forever workload
 
 fib :: Integer -> Integer
 fib 0 = 0
