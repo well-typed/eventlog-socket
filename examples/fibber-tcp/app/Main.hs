b7d2ace5ca16c694af64b17454f419bcf0863b30
Matthew Pickering <matthewtpickering@gmail.com>
2025-11-18 12:23:06 +0000

    Example tcp socket project

---
+++ examples/fibber-tcp/app/Main.hs
@@ -0,0 +1,32 @@
+module Main where
+
+import Control.Monad (forever)
+import Data.Foldable (for_)
+import Data.Maybe (fromMaybe)
+import GHC.Eventlog.Socket
+import System.Environment
+
+data Mode = Finite | Infinite
+
+parseArgs :: [String] -> (Mode, [String])
+parseArgs ("--forever" : rest) = (Infinite, rest)
+parseArgs args = (Finite, args)
+
+main :: IO ()
+main = do
+    fibberHost <- lookupEnv "FIBBER_EVENTLOG_TCP_HOST"
+    fibberPort <- fromMaybe "4242" <$> lookupEnv "FIBBER_EVENTLOG_TCP_PORT"
+    startTcp fibberHost fibberPort
+    wait
+    args <- getArgs
+    let (mode, fibArgs) = parseArgs args
+        workload = for_ fibArgs $ \arg -> print (fib (read arg))
+    case (mode, fibArgs) of
+        (_, []) -> putStrLn "Provide at least one integer argument."
+        (Finite, _) -> workload
+        (Infinite, _) -> forever workload
+
+fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n - 1) + fib (n - 2)
