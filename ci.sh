2bd47065e77ce7b1428609075e76cc99b5835851
Matthew Pickering <matthewtpickering@gmail.com>
2025-11-18 13:05:26 +0000

    Consolidate testing logic into a single script

--- ci.sh
+++ ci.sh
@@ -18,10 +18,11 @@ run_step() {
     fi
 }
 
-run_step "Test fibber (finite)" "$TESTS_DIR/test-normal.sh" unix fibber 30
-run_step "Test fibber (forever, with reconnect)" "$TESTS_DIR/test-reconnect.sh" unix fibber --forever 24
-run_step "Test fibber-tcp (finite)" "$TESTS_DIR/test-normal.sh" tcp fibber-tcp 24
-run_step "Test fibber-c-main (finite)" "$TESTS_DIR/test-normal.sh" unix fibber-c-main 30
-run_step "Test fibber-c-main (forever, with reconnect)" "$TESTS_DIR/test-reconnect.sh" unix fibber-c-main --forever 24
+run_step "Test fibber (finite)" "$TESTS_DIR/test-normal.sh" unix normal fibber 30
+run_step "Test fibber (forever, with reconnect)" "$TESTS_DIR/test-normal.sh" unix reconnect fibber --forever 24
+run_step "Test fibber-tcp (finite)" "$TESTS_DIR/test-normal.sh" tcp normal fibber-tcp 24
+run_step "Test fibber-tcp (forever, with-reconnect)" "$TESTS_DIR/test-normal.sh" tcp reconnect fibber-tcp --forever 24
+run_step "Test fibber-c-main (finite)" "$TESTS_DIR/test-normal.sh" unix normal fibber-c-main 30
+run_step "Test fibber-c-main (forever, with reconnect)" "$TESTS_DIR/test-normal.sh" unix reconnect fibber-c-main --forever 24
 
 printf '\nAll requested steps completed.\n'
