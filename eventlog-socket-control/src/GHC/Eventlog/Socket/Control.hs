dfcaa2e3f68b2675d290f9932f8f85f6d5fc439a
Wen Kokke <wenkokke@users.noreply.github.com>
2026-01-08 14:00:01 +0000

    Add namespaces to commands

--- eventlog-socket-control/src/GHC/Eventlog/Socket/Control.hs
+++ eventlog-socket-control/src/GHC/Eventlog/Socket/Control.hs
@@ -1,40 +1,92 @@
+{-# LANGUAGE PatternSynonyms #-}
+
 module GHC.Eventlog.Socket.Control (
-    Command (..),
+    Namespace,
+    CommandId (..),
+    Command (StartHeapProfiling, StopHeapProfiling, RequestHeapProfile),
+    userCommand,
 ) where
 
-import Control.Monad (unless)
+import Control.Exception (Exception (displayException), throw)
+import Control.Monad (unless, when)
 import Data.Binary (Binary (..), Get, Put, getWord8, putWord8)
 import Data.Foldable (traverse_)
+import Data.Word (Word8)
 import Text.Printf (printf)
 import Prelude hiding (getChar)
 
-data Command
-    = StartHeapProfiling
-    | StopHeapProfiling
-    | RequestHeapProfile
+userCommand :: Namespace -> CommandId -> Command
+userCommand namespace commandId
+    | namespace == BuiltinNamespace = throw CannotUseBuiltinNamespace
+    | otherwise = Command namespace commandId
+
+newtype Namespace = Namespace {unNamespace :: Word8}
     deriving (Eq, Show)
+
+pattern BuiltinNamespace :: Namespace
+pattern BuiltinNamespace = Namespace 0
+
+data CannotUseBuiltinNamespace = CannotUseBuiltinNamespace
+    deriving (Show)
+
+instance Exception CannotUseBuiltinNamespace
+
+newtype CommandId = CommandId {unCommandId :: Word8}
+    deriving (Eq, Show)
+
+nextBuiltinCommandId :: CommandId
+nextBuiltinCommandId = CommandId 3
+
+data UnknownBuiltinCommandId = UnknownBuiltinCommandId CommandId
+    deriving (Show)
+
+instance Exception UnknownBuiltinCommandId
+
+data Command = Command !Namespace !CommandId
+    deriving (Eq, Show)
+
+pattern StartHeapProfiling :: Command
+pattern StartHeapProfiling = Command BuiltinNamespace (CommandId 0)
+
+pattern StopHeapProfiling :: Command
+pattern StopHeapProfiling = Command BuiltinNamespace (CommandId 1)
+
+pattern RequestHeapProfile :: Command
+pattern RequestHeapProfile = Command BuiltinNamespace (CommandId 2)
 
 instance Binary Command where
     put :: Command -> Put
-    put command = do
+    put (Command namespace commandId) = do
         putList "GCTL"
-        case command of
-            StartHeapProfiling -> putWord8 1
-            StopHeapProfiling -> putWord8 2
-            RequestHeapProfile -> putWord8 3
+        putNamespace namespace
+        putCommandId commandId
 
     get :: Get Command
     get = do
         getString "GCTL"
-        getWord8 >>= \case
-            1 -> pure StartHeapProfiling
-            2 -> pure StopHeapProfiling
-            3 -> pure RequestHeapProfile
-            b -> fail $ printf "Unexpected %02x" b
+        namespace <- getNamespace
+        commandId <- getCommandId
+        when (namespace == BuiltinNamespace) $
+            when (unCommandId commandId >= unCommandId nextBuiltinCommandId) $
+                fail . displayException $
+                    UnknownBuiltinCommandId commandId
+        pure $ Command namespace commandId
 
 --------------------------------------------------------------------------------
 -- Internal helpers.
 --------------------------------------------------------------------------------
+
+putNamespace :: Namespace -> Put
+putNamespace = putWord8 . unNamespace
+
+getNamespace :: Get Namespace
+getNamespace = Namespace <$> getWord8
+
+putCommandId :: CommandId -> Put
+putCommandId = putWord8 . unCommandId
+
+getCommandId :: Get CommandId
+getCommandId = CommandId <$> getWord8
 
 getString :: String -> Get ()
 getString = traverse_ getChar
