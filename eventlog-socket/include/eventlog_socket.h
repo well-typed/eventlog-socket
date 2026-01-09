fdb9ab2490f9389e4c17a468905468b72e903247
Wen Kokke <wenkokke@users.noreply.github.com>
2026-01-09 13:19:30 +0000

    Move control to its own file.
    * Use static pthread initializers.
    * Use multiple mutexes.
    * Remove control_connection_closed.
    * Remove superfluous newlines from debug statements.
    * Add builtin handlers in initializer.
    * Move ready/armed logic into functions.
    * Use qualified public names.
    * Update custom-command-handler.c.
    * Mark control functions as hidden.

--- eventlog-socket/include/eventlog_socket.h
+++ eventlog-socket/include/eventlog_socket.h
@@ -8,14 +8,21 @@
 
 extern const EventLogWriter SocketEventLogWriter;
 
-typedef uint8_t control_namespace_t;
+typedef uint8_t eventlog_socket_control_namespace_id_t;
 
-typedef void (*eventlog_control_command_handler)(
-    control_namespace_t namespace_id, uint8_t cmd_id, void *user_data);
+typedef uint8_t eventlog_socket_control_command_id_t;
 
-bool eventlog_socket_register_control_command(
-    control_namespace_t namespace_id, uint8_t cmd_id,
-    eventlog_control_command_handler handler, void *user_data);
+typedef struct {
+  eventlog_socket_control_namespace_id_t namespace_id;
+  eventlog_socket_control_command_id_t command_id;
+} eventlog_socket_control_command_t;
+
+typedef void eventlog_socket_control_command_handler_t(
+    const eventlog_socket_control_command_t command, void *user_data);
+
+bool eventlog_socket_control_register_command(
+    eventlog_socket_control_command_t command,
+    eventlog_socket_control_command_handler_t *handler, void *user_data);
 
 void eventlog_socket_init_unix(const char *sock_path);
 void eventlog_socket_init_tcp(const char *host, const char *port);
@@ -29,4 +36,4 @@ void eventlog_socket_start_tcp(const char *host, const char *port, bool wait);
 int eventlog_socket_hs_main(int argc, char *argv[], RtsConfig conf,
                             StgClosure *main_closure);
 
-#endif
+#endif /* EVENGLOG_SOCKET_H */
