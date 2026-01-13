a052880e32df50d3d89fb5f56dd13c9d1c9ea68c
Wen Kokke <wenkokke@users.noreply.github.com>
2026-01-13 19:47:11 +0000

    Add HIDDEN macro for hidden symbols.

--- eventlog-socket/cbits/eventlog_socket/control.h
+++ eventlog-socket/cbits/eventlog_socket/control.h
@@ -1,18 +1,18 @@
 #ifndef EVENTLOG_SOCKET_CONTROL_H
 #define EVENTLOG_SOCKET_CONTROL_H
 
+#include "./macros.h"
+
 #include <pthread.h>
 #include <stdbool.h>
 
-void __attribute__((visibility("hidden"))) eventlog_socket_control_start(void);
+void HIDDEN eventlog_socket_control_start(void);
 
-void __attribute__((visibility("hidden")))
-eventlog_socket_control_create(const volatile int *control_fd_ptr,
-                               pthread_mutex_t *control_fd_mutex_ptr);
+void HIDDEN eventlog_socket_control_create(
+    const volatile int *control_fd_ptr, pthread_mutex_t *control_fd_mutex_ptr);
 
-void __attribute__((visibility("hidden"))) eventlog_socket_control_arm(void);
+void HIDDEN eventlog_socket_control_arm(void);
 
-bool __attribute__((visibility("hidden")))
-eventlog_socket_control_is_armed(void);
+bool HIDDEN eventlog_socket_control_is_armed(void);
 
 #endif /* EVENTLOG_SOCKET_CONTROL_H */
