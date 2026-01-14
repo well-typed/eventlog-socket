597bee110b254a298d151bfd2ab324714f283677
Wen Kokke <wenkokke@users.noreply.github.com>
2026-01-14 15:17:13 +0000

    Add eventlog_socket_control_register_namespace.

--- eventlog-socket/include/eventlog_socket.h
+++ eventlog-socket/include/eventlog_socket.h
@@ -20,6 +20,10 @@ typedef struct {
 typedef void eventlog_socket_control_command_handler_t(
     const eventlog_socket_control_command_t command, void *user_data);
 
+bool eventlog_socket_control_register_namespace(
+    size_t namespace_len, const char namespace[namespace_len + 1],
+    eventlog_socket_control_namespace_id_t *namespace_id_out);
+
 bool eventlog_socket_control_register_command(
     eventlog_socket_control_command_t command,
     eventlog_socket_control_command_handler_t *handler, void *user_data);
