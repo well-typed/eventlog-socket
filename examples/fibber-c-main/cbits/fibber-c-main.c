5b43c1743d1695e242fa51b7314e4c1bb8958b43
Wen Kokke <wenkokke@users.noreply.github.com>
2026-01-16 15:34:56 +0000

    Use eventlog_socket_init_from_env in fibber-c-main

--- examples/fibber-c-main/cbits/fibber-c-main.c
+++ examples/fibber-c-main/cbits/fibber-c-main.c
@@ -15,14 +15,7 @@ int main(int argc, char *argv[]) {
   conf.rts_opts_enabled = RtsOptsAll;
 
   // If GHC_EVENTLOG_UNIX_SOCKET is set...
-  const char *sock_path = getenv("GHC_EVENTLOG_UNIX_SOCKET");
-  if (sock_path != NULL) {
-    // Start the eventlog writer.
-    eventlog_socket_init_unix(sock_path);
-
-    // Wait for the monitoring process to connect.
-    eventlog_socket_wait();
-  }
+  eventlog_socket_init_from_env();
 
   // Delegate to the helper that runs hs_main and the application closure.
   eventlog_socket_hs_main(argc, argv, conf, &ZCMain_main_closure);
