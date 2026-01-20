3526ebd567b9d8c8948845519df1b7f9c0a7049f
Wen Kokke <wenkokke@users.noreply.github.com>
2026-01-20 18:13:18 +0000

    Fix debug macros

--- eventlog-socket/cbits/eventlog_socket/debug.h
+++ eventlog-socket/cbits/eventlog_socket/debug.h
@@ -26,42 +26,75 @@
 
 #endif /* DEBUG_COLOR */
 
-#define DEBUG_PREFIX(tag, color)                                               \
-  printf(color "%s[%s|%d|%s]: " DEBUG_COLOR_RESET, tag, __FILE__, __LINE__,    \
-         __func__)
+#define DEBUG_VERBOSITY_QUIET 0
+#define DEBUG_VERBOSITY_ERROR 1
+#define DEBUG_VERBOSITY_WARN 2
+#define DEBUG_VERBOSITY_INFO 3
+#define DEBUG_VERBOSITY_DEBUG 4
+#define DEBUG_VERBOSITY_TRACE 5
 
-#ifdef DEBUG
-
-#define DEBUG_TRACE(...)                                                       \
-  do {                                                                         \
-    DEBUG_PREFIX("TRACE", DEBUG_COLOR_BLUE);                                   \
-    fprintf(stderr, __VA_ARGS__);                                              \
-    fprintf(stderr, "\n");                                                     \
-  } while (0)
-#define DEBUG_WARN(...)                                                        \
-  do {                                                                         \
-    DEBUG_PREFIX("WARNING", DEBUG_COLOR_YELLOW);                               \
-    fprintf(stderr, __VA_ARGS__);                                              \
-    fprintf(stderr, "\n");                                                     \
-  } while (0)
-#define DEBUG_ERROR(...)                                                       \
-  do {                                                                         \
-    DEBUG_PREFIX("ERROR", DEBUG_COLOR_RED);                                    \
-    fprintf(stderr, __VA_ARGS__);                                              \
-    fprintf(stderr, "\n");                                                     \
-  } while (0)
-#define DEBUG_ERRNO(s)                                                         \
-  do {                                                                         \
-    DEBUG_PREFIX("ERROR", DEBUG_COLOR_RED);                                    \
-    perror(s);                                                                 \
-  } while (0)
-
+#ifdef DEBUG_VERBOSITY
 #else
-
-#define DEBUG_TRACE(...) ((void)0)
-#define DEBUG_WARN(...) ((void)0)
-#define DEBUG_ERROR(...) ((void)0)
-#define DEBUG_ERRNO(s) ((void)0)
-
+#ifdef DEBUG
+#define DEBUG_VERBOSITY DEBUG_VERBOSITY_TRACE
+#else
+#define DEBUG_VERBOSITY DEBUG_VERBOSITY_ERROR
 #endif /* DEBUG */
+#endif /* DEBUG_VERBOSITY */
+
+#define DEBUG_TRACE(fmt, ...)                                                  \
+  do {                                                                         \
+    if (DEBUG_VERBOSITY >= DEBUG_VERBOSITY_TRACE) {                            \
+      fprintf(stderr,                                                          \
+              DEBUG_COLOR_PURPLE "TRACE[%s|%d|%s]: " DEBUG_COLOR_RESET fmt     \
+                                 "\n",                                         \
+              __FILE__, __LINE__, __func__, __VA_ARGS__);                      \
+    }                                                                          \
+  } while (0)
+
+#define DEBUG_DEBUG(fmt, ...)                                                  \
+  do {                                                                         \
+    if (DEBUG_VERBOSITY >= DEBUG_VERBOSITY_DEBUG) {                            \
+      fprintf(stderr,                                                          \
+              DEBUG_COLOR_BLUE "DEBUG[%s|%d|%s]: " DEBUG_COLOR_RESET fmt "\n", \
+              __FILE__, __LINE__, __func__, __VA_ARGS__);                      \
+    }                                                                          \
+  } while (0)
+
+#define DEBUG_INFO(fmt, ...)                                                   \
+  do {                                                                         \
+    if (DEBUG_VERBOSITY >= DEBUG_VERBOSITY_INFO) {                             \
+      fprintf(stderr,                                                          \
+              DEBUG_COLOR_YELLOW "INFO[%s|%d|%s]: " DEBUG_COLOR_RESET fmt      \
+                                 "\n",                                         \
+              __FILE__, __LINE__, __func__, __VA_ARGS__);                      \
+    }                                                                          \
+  } while (0)
+
+#define DEBUG_WARN(fmt, ...)                                                   \
+  do {                                                                         \
+    if (DEBUG_VERBOSITY >= DEBUG_VERBOSITY_WARN) {                             \
+      fprintf(stderr,                                                          \
+              DEBUG_COLOR_YELLOW "WARN[%s|%d|%s]: " DEBUG_COLOR_RESET fmt      \
+                                 "\n",                                         \
+              __FILE__, __LINE__, __func__, __VA_ARGS__);                      \
+    }                                                                          \
+  } while (0)
+
+#define DEBUG_ERROR(fmt, ...)                                                  \
+  do {                                                                         \
+    if (DEBUG_VERBOSITY >= DEBUG_VERBOSITY_ERROR) {                            \
+      fprintf(stderr,                                                          \
+              DEBUG_COLOR_RED "ERROR[%s|%d|%s]: " DEBUG_COLOR_RESET fmt "\n",  \
+              __FILE__, __LINE__, __func__, __VA_ARGS__);                      \
+    }                                                                          \
+  } while (0)
+
+#define DEBUG_ERRNO(msg)                                                       \
+  do {                                                                         \
+    if (DEBUG_VERBOSITY >= DEBUG_VERBOSITY_ERROR) {                            \
+      perror(DEBUG_COLOR_RED "ERROR: " DEBUG_COLOR_RESET msg);                 \
+    }                                                                          \
+  } while (0)
+
 #endif /* EVENTLOG_SOCKET_DEBUG_H */
