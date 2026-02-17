607dd33cc666b9059760d7f56785597993e320b6
Wen Kokke <wenkokke@users.noreply.github.com>
2026-02-17 17:26:35 +0000

    Add examples to documentation and tidy up fibber-c-main and custom-command-c-main.

--- examples/custom-command/cbits/custom-command-c-main.c
+++ examples/custom-command/cbits/custom-command-c-main.c
@@ -1,7 +1,6 @@
-#include <stdlib.h>
-
 #include <Rts.h>
 #include <eventlog_socket.h>
+#include <stdlib.h>
 #include <string.h>
 
 // Debug macros
@@ -92,40 +91,49 @@ void custom_command_init(void) {
 extern StgClosure ZCMain_main_closure;
 
 int main(int argc, char *argv[]) {
-  RtsConfig rts_config = defaultRtsConfig;
-  rts_config.rts_opts_enabled = RtsOptsAll;
-  rts_config.rts_opts = "-l";
+
+  // Create a GHC RTS configuration object.
+  RtsConfig rts_config = {0};
+  memcpy(&rts_config, &defaultRtsConfig, sizeof(RtsConfig));
+  rts_config.rts_opts_enabled = RtsOptsAll; // Enable all RTS options.
+  rts_config.rts_opts = "-l";               // Enable binary eventlog.
 
   custom_command_init();
 
-  // Start with configuration from the environment...
+  // Read the socket address and options from the environment.
   EventlogSocketAddr eventlog_socket_addr = {0};
   EventlogSocketOpts eventlog_socket_opts = {0};
   const EventlogSocketFromEnvStatus status =
       eventlog_socket_from_env(&eventlog_socket_addr, &eventlog_socket_opts);
+
+  // Handle the return status.
   switch (status) {
   case EVENTLOG_SOCKET_FROM_ENV_OK:
+    // Initialise eventlog socket.
     eventlog_socket_init(&eventlog_socket_addr, &eventlog_socket_opts);
-    goto free;
+    break;
   case EVENTLOG_SOCKET_FROM_ENV_NONE:
-    goto main; // skip free
+    goto main; // Skip free.
   case EVENTLOG_SOCKET_FROM_ENV_INVAL:
-    goto main; // skip free
+    goto main; // Skip free.
   case EVENTLOG_SOCKET_FROM_ENV_UNIX_PATH_TOO_LONG:
     assert(eventlog_socket_addr.esa_tag == EVENTLOG_SOCKET_UNIX);
     DEBUG_ERROR("value of %s (%s) is too long", EVENTLOG_SOCKET_ENV_UNIX_PATH,
                 eventlog_socket_addr.esa_unix_addr.esa_unix_path);
-    goto free;
+    break;
   case EVENTLOG_SOCKET_FROM_ENV_INET_HOST_MISSING:
     DEBUG_ERROR("no value given for %s", EVENTLOG_SOCKET_ENV_INET_HOST);
-    goto free;
+    break;
   case EVENTLOG_SOCKET_FROM_ENV_INET_PORT_MISSING:
     DEBUG_ERROR("no value given for %s", EVENTLOG_SOCKET_ENV_INET_PORT);
-    goto free;
+    break;
   }
-free:
+
+  // Free the memory held by socket address and options.
   eventlog_socket_addr_free(&eventlog_socket_addr);
   eventlog_socket_opts_free(&eventlog_socket_opts);
+
 main:
-  eventlog_socket_wrap_hs_main(argc, argv, rts_config, &ZCMain_main_closure);
+  return eventlog_socket_wrap_hs_main(argc, argv, rts_config,
+                                      &ZCMain_main_closure);
 }
