4018a3f581d1f3f6f9c14310ae19f2e8e54cc8bb
Wen Kokke <wenkokke@users.noreply.github.com>
2026-03-16 15:18:35 +0000

    Improve test runner.

--- scripts/test-haskell.sh
+++ scripts/test-haskell.sh
@@ -1,3 +1,31 @@
-#!/bin/sh
+#!/bin/sh -e
 
-cabal clean && cabal test eventlog-socket-tests -f+debug "$@" 2>eventlog-socket-tests.log
+# Usage: ./scripts/test-haskell.sh [CABAL_ARGS] -- [TEST_ARGS]
+#
+# NOTE: If environment variable DEBUG is defined, this script logs both the
+#       output and error streams of the test suite to files and shows only
+#       the error stream. Otherwise, it shows only the output stream and logs
+#       only the error stream.
+
+# Get the project root directory:
+ROOT_DIR="$(CDPATH= cd -- "$(dirname "$(dirname "$(dirname -- "$0")")")" && pwd)"
+
+# Log file for stderr.
+ERR_FILE="${ROOT_DIR}/eventlog-socket-tests.err.log"
+
+# Run test command.
+if [ -n "${DEBUG+x}" ]; then
+	# Log file for stdout.
+	OUT_FILE="${ROOT_DIR}/eventlog-socket-tests.out.log"
+
+	# Pipe for stderr.
+	ERR_FIFO="${TMPDIR:-/tmp}/eventlog-socket-tests.err.$$"
+	mkfifo "${ERR_FIFO}"
+	trap 'rm "${ERR_FIFO}"' EXIT
+	tee "${ERR_FILE}" <"${ERR_FIFO}" >&2 &
+
+	# Run test suite and log debug information.
+	cabal run eventlog-socket-tests -f+debug "$@" >"${OUT_FILE}" 2>"${ERR_FIFO}"
+else
+	cabal run eventlog-socket-tests -f+debug "$@" 2>"${ERR_FILE}"
+fi
