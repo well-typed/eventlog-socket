5d657af994a5bdb4c3389a396cf5929df7052f6f
Wen Kokke <wenkokke@users.noreply.github.com>
2026-03-17 16:40:13 +0000

    Rewrite RETURN_ON_ERROR to filter out successful getaddrinfo and system calls.

--- eventlog-socket/cbits/eventlog_socket/error.h
+++ eventlog-socket/cbits/eventlog_socket/error.h
@@ -23,24 +23,29 @@
   ((EventlogSocketStatus){.ess_status_code = EVENTLOG_SOCKET_ERR_GAI,          \
                           .ess_error_code = (gai_error)})
 
+/// @brief A condition that evaluates to @c true if @p expr is an error.
+///
+/// @warning This macro evaluates its first argument multiple times.
+#define STATUS_IS_ERROR(expr)                                                  \
+  /* If none of the following are true... */                                   \
+  (!((/* ...the status is an OK. */                                            \
+      (expr).ess_status_code == EVENTLOG_SOCKET_OK) ||                         \
+     (/* ...the status is a successful getaddrinfo exit code. */               \
+      (expr).ess_status_code == EVENTLOG_SOCKET_ERR_GAI &&                     \
+      (expr).ess_error_code == 0) ||                                           \
+     (/* ...the status is a successful system call exit code. */               \
+      (expr).ess_status_code == EVENTLOG_SOCKET_ERR_SYS &&                     \
+      (expr).ess_error_code == 0)))
+
 /// @brief If the @p expr returns an error status, immediately return it.
 #define RETURN_ON_ERROR(expr)                                                  \
   do {                                                                         \
     const EventlogSocketStatus status = (expr);                                \
-    if (status.ess_status_code != EVENTLOG_SOCKET_OK) {                        \
+    if (STATUS_IS_ERROR(status)) {                                             \
       char *strerr = eventlog_socket_strerror(status);                         \
       DEBUG_ERROR("%s", strerr);                                               \
       free(strerr);                                                            \
       return status;                                                           \
-    }                                                                          \
-  } while (0)
-
-/// @brief If the @p expr returns a pthread error code, immediately return it.
-#define RETURN_ON_ERROR_PTHREAD(expr)                                          \
-  do {                                                                         \
-    const int success_or_errno = (expr);                                       \
-    if (success_or_errno != 0) {                                               \
-      RETURN_ON_ERROR(STATUS_FROM_PTHREAD_ERROR(success_or_errno));            \
     }                                                                          \
   } while (0)
 
