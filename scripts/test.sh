afcb29c73eedc13daa7c3444d57a9bec74cb90d4
Wen Kokke <wenkokke@users.noreply.github.com>
2026-04-06 13:41:01 +0100

    Standardise test.sh.

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,16 +7,41 @@
 #       the error stream. Otherwise, it shows only the output stream and logs
 #       only the error stream.
 
-# Get the project root directory:
-ROOT_DIR="$(CDPATH= cd -- "$(dirname "$(dirname "$(dirname -- "$0")")")" && pwd)"
+# Find the repository root directory
+REPO_ROOT_DIR="$(CDPATH= cd -- "$(dirname -- "$(dirname -- "$0")")" && pwd)"
+
+# Read the expected version:
+EXPECT_VERSION="$(awk -F'=' '/^cabal=/{print$2}' ./scripts/dev-dependencies.txt)"
+
+# Find cabal:
+#
+# 1. Use CABAL if it is set.
+# 2. Look for cabal-$EXPECTED_VERSION.
+# 3. Look for cabal.
+#
+if [ "${CABAL}" = "" ]; then
+  if ! CABAL="$(which "cabal-${EXPECT_VERSION}")"; then
+    if ! CABAL="$(which "cabal")"; then
+      echo "Requires cabal ${EXPECT_VERSION}; no version found"
+      exit 1
+    fi
+  fi
+fi
+
+# Check cabal version:
+ACTUAL_VERSION="$("${CABAL}" --numeric-version | head -n 1)"
+if [ "${ACTUAL_VERSION}" != "${EXPECT_VERSION}" ]; then
+  # Version mismatch is never an error:
+  echo "Requires cabal ${EXPECT_VERSION}; version ${ACTUAL_VERSION} found"
+fi
 
 # Log file for stderr.
-ERR_FILE="${ROOT_DIR}/eventlog-socket-tests.err.log"
+ERR_FILE="${REPO_ROOT_DIR}/eventlog-socket-tests.err.log"
 
 # Run test command.
 if [ -n "${DEBUG+x}" ]; then
 	# Log file for stdout.
-	OUT_FILE="${ROOT_DIR}/eventlog-socket-tests.out.log"
+	OUT_FILE="${REPO_ROOT_DIR}/eventlog-socket-tests.out.log"
 
 	# Pipe for stderr.
 	ERR_FIFO="${TMPDIR:-/tmp}/eventlog-socket-tests.err.$$"
@@ -25,7 +50,7 @@ if [ -n "${DEBUG+x}" ]; then
 	tee "${ERR_FILE}" <"${ERR_FIFO}" >&2 &
 
 	# Run test suite and log debug information.
-	cabal run eventlog-socket-tests --enable-tests -f+debug "$@" >"${OUT_FILE}" 2>"${ERR_FIFO}"
+	${CABAL} run eventlog-socket-tests --enable-tests -f+debug "$@" >"${OUT_FILE}" 2>"${ERR_FIFO}"
 else
-	cabal run eventlog-socket-tests --enable-tests -f+debug "$@" 2>"${ERR_FILE}"
+	${CABAL} run eventlog-socket-tests --enable-tests -f+debug "$@" 2>"${ERR_FILE}"
 fi
